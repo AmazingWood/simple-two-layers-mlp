1e59781cb2e184da55f54a800df03b2c76c59b2f
AmazingWood <amazing-wood@outlook.com>
2020-01-14 08:44:34 +0000

    leaky relu

--- Layers/Non_Opt_Activations_C_impl.c
+++ Layers/Non_Opt_Activations_C_impl.c
@@ -1,14 +1,27 @@
 #include "Non_Opt_Activations_C_impl.h"
 
-void batchLeakyForward(const float* input,float* output,const int32_t* size){
+void batchReluForward(const float* input,float* output,const int32_t* size){
     for (int32_t i = 0; i < *size; i++)
     {
         output[i]=relu_activate(*input);
     }
 }
-void batchLeakyBackward(const float* input,float* output,const int32_t* size){
+void batchReluBackward(const float* input,float* output,const int32_t* size){
     for (int32_t i = 0; i < *size; i++)
     {
         output[i]=relu_gradient(*input);
+    }
+}
+
+void batchLeakyForward(const float* input,float* output,const int32_t* size){
+    for (int32_t i = 0; i < *size; i++)
+    {
+        output[i]=leaky_activate(*input);
+    }
+}
+void batchLeakyBackward(const float* input,float* output,const int32_t* size){
+    for (int32_t i = 0; i < *size; i++)
+    {
+        output[i]=leaky_gradient(*input);
     }
 }
--- Layers/Non_Opt_Activations_C_impl.h
+++ Layers/Non_Opt_Activations_C_impl.h
@@ -4,5 +4,8 @@ static inline float relu_gradient(float x){return (x>0);}
 static inline float leaky_activate(float x){return (x>0) ? x : .1*x;}
 static inline float leaky_gradient(float x){return (x>0) ? 1 : .1;}
 
+void batchReluForward(const float* input,float* output,const int32_t* size);
+void batchReluBackward(const float* input,float* output,const int32_t* size);
+
 void batchLeakyForward(const float* input,float* output,const int32_t* size);
 void batchLeakyBackward(const float* input,float* output,const int32_t* size);
